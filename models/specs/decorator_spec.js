60ee5987aa2072f5e9adc7d417cfbb34fa5ddc99
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-06 16:33:43 +0000

    end of lab

--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -1,10 +1,13 @@
 const assert = require('assert');
 const Decorator = require('../decorator.js');
+const Paint = require('../paint.js');
 
 describe('Decorator', function(){
     let decorator;
+    let paint;
     beforeEach(function(){
         decorator = new Decorator('Bob');
+        paint = new Paint('red', 4);
     });
     it('should have a name', function(){
         const actual = decorator.name;
@@ -13,5 +16,10 @@ describe('Decorator', function(){
     it('should start with emoty stock', function(){
         const actual = decorator.paintStock;
         assert.deepStrictEqual(actual, []);
+    });
+    it('should be able to add paint', function(){
+        const actual = decorator.addPaint(paint);
+        assert.deepStrictEqual(actual, ['red'])
+
     });
 });
